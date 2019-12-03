511384bc4590f8245ea7f9abcc9a2dbb46f555c3
Shivam Ratan <shivamratan@github.com>
2019-12-03 20:07:59 +0530

    Misc construction

--- app/src/main/java/com/ratanapps/databindingsample/ui/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/ratanapps/databindingsample/ui/ui/slideshow/SlideshowFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
